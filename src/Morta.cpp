1964267bd1cf32f9eb38d4ffcd504fad553f87bd
Dan Green <danngreen1@gmail.com>
2025-04-09 22:24:39 -0700

    Revert Morta jack text to upstream

--- src/Morta.cpp
+++ src/Morta.cpp
@@ -60,25 +60,27 @@ struct Morta : Module {
         // Configure the inputs
         configInput(MAIN_INPUT, "Main");
         configInput(RANGE_CV_INPUT, "Range CV");
+        
+        configOutput(OUTPUT_1_1, "0▸1" );
+        configOutput(OUTPUT_1_2, "0▸5" );
+        configOutput(OUTPUT_1_3, "0▸10");
+        configOutput(OUTPUT_1_4, "0▸R" );
 
-        configOutput(OUTPUT_1_1, "0 ▸ 1 Out");
-        configOutput(OUTPUT_1_2, "0 ▸ 5 Out");
-        configOutput(OUTPUT_1_3, "0 ▸ 10 Out");
-        configOutput(OUTPUT_1_4, "0 ▸ R Out");
-        configOutput(OUTPUT_2_1, "-1 ▸ 1 Out");
-        configOutput(OUTPUT_2_2, "-5 ▸ 5 Out");
-        configOutput(OUTPUT_2_3, "-10 ▸ 10 Out");
-        configOutput(OUTPUT_2_4, "-R ▸ R Out");
-        configOutput(OUTPUT_3_1, "1 ▸ -1 Out");
-        configOutput(OUTPUT_3_2, "-5 ▸ 5 Out");
-        configOutput(OUTPUT_3_3, "10 ▸ -10 Out");
-        configOutput(OUTPUT_3_4, "R ▸ -R Out");
-        configOutput(OUTPUT_4_1, "1 ▸ 0 Out");
-        configOutput(OUTPUT_4_2, "5 ▸ 0 Out");
-        configOutput(OUTPUT_4_3, "10 ▸ 0 Out");
-        configOutput(OUTPUT_4_4, "R ▸ 0 Out");
-        configOutput(MAIN_OUTPUT, "Main Out");
+        configOutput(OUTPUT_2_1, "-1▸1" );
+        configOutput(OUTPUT_2_2, "-5▸5" );
+        configOutput(OUTPUT_2_3, "-10▸10");
+        configOutput(OUTPUT_2_4, "-R▸R" );
 
+        configOutput(OUTPUT_3_1, "1▸-1" );
+        configOutput(OUTPUT_3_2, "5▸-5" );
+        configOutput(OUTPUT_3_3, "10▸-10");
+        configOutput(OUTPUT_3_4, "R▸-R" );
+
+        configOutput(OUTPUT_4_1, "1▸0" );
+        configOutput(OUTPUT_4_2, "5▸0" );
+        configOutput(OUTPUT_4_3, "10▸0");
+        configOutput(OUTPUT_4_4, "R▸0" );
+      
         isEditing[0] = false; // Initialize editing state to false    
     }
 
