fc81a7596e41d842480415351e5788bbe48bffa2
Dan Green <danngreen1@gmail.com>
2025-10-29 22:10:14 -0700

    Re-enable Weave for MM

--- src/plugin.cpp
+++ src/plugin.cpp
@@ -38,9 +38,7 @@ void init(Plugin* p) {
     p->addModel(modelJunkDNA);
     p->addModel(modelPicus);
     p->addModel(modelNode);
-#if !defined(METAMODULE)
     p->addModel(modelWeave);
-#endif
     p->addModel(modelWonk);
     p->addModel(modelHammer);
     p->addModel(modelHub);
